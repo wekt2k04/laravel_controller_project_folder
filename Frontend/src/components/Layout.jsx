a7fb54704c96cb269b9e364c0124591b5f14fae4
safaa <nadirsafaa9@gmail.com>
2025-12-26 23:47:34 +0100

    Add Layout component with Sidebar and Outlet

--- Frontend/src/components/Layout.jsx
+++ Frontend/src/components/Layout.jsx
@@ -1,1 +1,30 @@
+import Sidebar from "./Sidebar";
+import { Outlet } from "react-router-dom";
+
+function Layout() {
+  return (
+    <div className="flex min-h-screen">
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* Content */}
+      <main
+        className="flex-1 p-6 bg-cover bg-center relative"
+        style={{
+          backgroundImage: "url('/dashboard-bg.png')",
+        }}
+      >
+        {/* overlay باش الكتابة تبان */}
+        <div className="absolute inset-0 bg-white/80"></div>
+
+        {/* real content */}
+        <div className="relative z-10">
+          <Outlet />
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Layout;
 
